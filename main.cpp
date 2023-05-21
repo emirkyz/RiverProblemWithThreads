12efece60f71688d929d7c482d02c6ab87742d51
emirkyz <402957@fen.ktu.edu.tr>
2023-05-21 21:40:12 +0300

    Duplication problem is solved with 2 methods; 1) Adding another seamphore for rowBoat() function. 2) First decreasing number of people and afterwards calling rowBoat()

--- main.cpp
+++ main.cpp
@@ -11,12 +11,11 @@
 #define N 4
 
 
-
-sem_t bilet,disla, test;
+sem_t disla, kalkis;
 int hackers = 0;
 int microsoftlar = 0;
-sem_t hacker_queue, microsoft_queue;
-bool is_captain = false;
+sem_t siradaki_hacker, siradaki_microsoft;
+bool bot_kalkti = false;
 
 int formasyon[2];
 int bot_sayisi = 1;
@@ -32,12 +31,11 @@ std::uniform_int_distribution<int> uni(1,6); // Guaranteed unbiased
 
 int main(){
 
-
-    sem_init(&test,0,1);
+    sem_init(&kalkis, 0, 1);
     sem_init(&disla,0,1);
-    sem_init(&bilet,0,24);
-    sem_init(&microsoft_queue,0,1);
-    sem_init(&hacker_queue,0,1);
+//    sem_init(&bilet,0,24);
+    sem_init(&siradaki_microsoft, 0, 1);
+    sem_init(&siradaki_hacker, 0, 1);
     pthread_t Microsoft[N], Hacker[N];
 
     for (int i = 0; i< N ; i++) {
@@ -56,15 +54,15 @@ int main(){
     }
 
     sem_destroy(&disla);
-    sem_destroy(&bilet);
-    sem_destroy(&microsoft_queue);
-    sem_destroy(&hacker_queue);
-    sem_destroy(&test);
+//    sem_destroy(&bilet);
+    sem_destroy(&siradaki_microsoft);
+    sem_destroy(&siradaki_hacker);
+    sem_destroy(&kalkis);
 }
 
 
 void rowBoat(std::string grup){
-    sem_wait(&test);
+    sem_wait(&kalkis);
     if (formasyon[0] == 0 and formasyon[1] == 0){
         return;
 //        printf("Bot kalkamaz\n");
@@ -74,30 +72,27 @@ void rowBoat(std::string grup){
     if (formasyon[0] == 4 and formasyon[1] == 0){
         printf("Microsoft grubu %d. bot ile kalkış gerçekleştirdi (%d,%d)\n",bot_sayisi,formasyon[0],formasyon[1]);
         bot_sayisi += 1;
-        sem_post(&test);
+        sem_post(&kalkis);
 
     }
     else if (formasyon[0] == 0 and formasyon[1] == 4){
         printf("Hacker gurubu %d. bot ile kalkış gerçekliştirdi(%d,%d)\n",bot_sayisi,formasyon[0],formasyon[1]);
         bot_sayisi += 1;
-        sem_post(&test);
+        sem_post(&kalkis);
 
     }
     else if (formasyon[0] == 2 and formasyon[1] == 2){
         printf("Hacker ve microsoft çalışanları birlikte %d. bot ile kalkış gerçekleştirdi (%d,%d)\n",bot_sayisi,formasyon[0],formasyon[1]);
         bot_sayisi += 1;
-        sem_post(&test);
+        sem_post(&kalkis);
 
     }
     else {
         printf("else içi");
     }
 
-
 //    printf("%s grubu bot ile  kalkış gerçekleştirdi\n",grup.c_str());
 //    printf("Formasyon = %d, %d\n",formasyon[0],formasyon[1]);
-
-
 
 }
 
@@ -112,7 +107,6 @@ void board(std::string kisi){
         bot_hacker -= 4;
         rowBoat("Hacker");
 //        bot_hacker = 0;
-
     }
     if (kisi == "Microsoft"){
         bot_microsoft +=1;
@@ -123,12 +117,10 @@ void board(std::string kisi){
         bot_microsoft -= 4 ;
         rowBoat("Microsoft");
 //        bot_microsoft = 0;
-
     }
     if (bot_microsoft == 2 and bot_hacker == 2){
         rowBoat("TEST");
     }
-
 }
 
 
@@ -144,39 +136,39 @@ void *microsoft_thread(void *){
 
         microsoftlar += 1;
         if (microsoftlar == 4) {
-            sem_post(&microsoft_queue);
-            sem_post(&microsoft_queue);
-            sem_post(&microsoft_queue);
-            sem_post(&microsoft_queue);
+            sem_post(&siradaki_microsoft);
+            sem_post(&siradaki_microsoft);
+            sem_post(&siradaki_microsoft);
+            sem_post(&siradaki_microsoft);
 
             formasyon[0] = 4;
             formasyon[1] = 0;
 
             microsoftlar -= 4;
-            is_captain = true;
+            bot_kalkti = true;
         } else if (hackers == 2 and microsoftlar == 2) {
-            sem_post(&microsoft_queue);
-            sem_post(&microsoft_queue);
-            sem_post(&hacker_queue);
-            sem_post(&hacker_queue);
+            sem_post(&siradaki_microsoft);
+            sem_post(&siradaki_microsoft);
+            sem_post(&siradaki_hacker);
+            sem_post(&siradaki_hacker);
 
             formasyon[0] = 2;
             formasyon[1] = 2;
 
             microsoftlar -= 2;
             hackers -=2;
-            is_captain = true;
+            bot_kalkti = true;
         } else {
             sem_post(&disla);
         }
 
 
-        sem_wait(&microsoft_queue);
+        sem_wait(&siradaki_microsoft);
 
         board("Microsoft");
 
 
-        if (is_captain) {
+        if (bot_kalkti) {
 
             sem_post(&disla);
         }
@@ -191,37 +183,36 @@ void *hacker_thread(void *){
 
         hackers += 1;
         if (hackers == 4) {
-            sem_post(&hacker_queue);
-            sem_post(&hacker_queue);
-            sem_post(&hacker_queue);
-            sem_post(&hacker_queue);
+            sem_post(&siradaki_hacker);
+            sem_post(&siradaki_hacker);
+            sem_post(&siradaki_hacker);
+            sem_post(&siradaki_hacker);
 
             formasyon[0] = 0;
             formasyon[1] = 4;
 
             hackers -= 4;
-            is_captain = true;
+            bot_kalkti = true;
         } else if (hackers == 2 and microsoftlar == 2) {
-            sem_post(&microsoft_queue);
-            sem_post(&microsoft_queue);
-            sem_post(&hacker_queue);
-            sem_post(&hacker_queue);
+            sem_post(&siradaki_microsoft);
+            sem_post(&siradaki_microsoft);
+            sem_post(&siradaki_hacker);
+            sem_post(&siradaki_hacker);
 
             formasyon[0] = 2;
             formasyon[1] = 2;
 
             hackers -= 2;
             microsoftlar -=2;
-            is_captain = true;
+            bot_kalkti = true;
         } else {
             sem_post(&disla);
         }
 
-        sem_wait(&hacker_queue);
-
+        sem_wait(&siradaki_hacker);
         board("Hacker");
 
-        if (is_captain) {
+        if (bot_kalkti) {
             sem_post(&disla);
         }
         sleep(1);
